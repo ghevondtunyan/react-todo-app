fd37015f396a85e07e231de242a1fa98b9939c73
ghevondtunyan <60566859+ghevondtunyan@users.noreply.github.com>
2020-10-21 10:02:31 +0400

    Updates

---
+++ precache-manifest.31cd0ef0164fe29af5d585ac44c53c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe0d6470809a9043a49673ce1faeb81",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "3c04e672c4e3ccb950ca",
+    "url": "/react-todo-app/static/css/main.1007fedb.chunk.css"
+  },
+  {
+    "revision": "4701125872232ca5f4f7",
+    "url": "/react-todo-app/static/js/2.71cd1550.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-todo-app/static/js/2.71cd1550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c04e672c4e3ccb950ca",
+    "url": "/react-todo-app/static/js/main.0229cb8b.chunk.js"
+  },
+  {
+    "revision": "a92c009c4532752e93cf",
+    "url": "/react-todo-app/static/js/runtime-main.2d557662.js"
+  }
+]);
